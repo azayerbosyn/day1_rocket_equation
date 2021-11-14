122cffcb5ace1af8ae7aaf31dcaf4e6c56a44c88
Azamat Erbosyn <Erbosyn75@gmail.com>
2021-11-14 12:57:29 +0600

    part 1

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+INPUT_MASS = [124846, 99745, 110203, 140165, 110228, 65706, 128481, 75921, 57331, 72951, 133413, 99524, 79546, 54653,
+              55166, 66215, 147696, 91054, 64752, 76311, 139572, 61110, 65846, 121489, 147534, 66591, 109963, 83412,
+              138965, 70102, 128844, 141002, 77655, 68539, 128687, 70559, 140747, 51397, 117550, 91515, 60960, 133280,
+              83244, 106644, 100333, 67608, 118120, 60024, 115547, 136229, 108403, 128776, 109599, 111189, 98538,
+              129715, 116630, 120772, 80105, 52489, 130247, 144003, 85226, 83769, 137921, 54737, 126406, 108756,
+              149633, 138201, 78980, 126909, 125768, 86214, 54873, 97723, 92677, 120405, 143317, 102981, 142668,
+              100398, 67258, 126583, 114611, 102525, 115205, 78329, 140703, 136978, 94465, 129510, 81039, 141997,
+              120643, 55377, 89966, 113672, 112665, 51323]
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def count_fuel(mass):
+    return mass // 3 - 2
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def total_fuel():
+    total_fuel_mass = 0
+    for mass in INPUT_MASS:
+        count = count_fuel(mass)
+        if count <= 0:
+            return total_fuel_mass
+        else:
+            total_fuel_mass += count
+    print(total_fuel_mass)
+    return total_fuel_mass
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+total_fuel()
+# part 1 answer: 3405721
